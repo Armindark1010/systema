bbbccecd8db08958108d339191f0c1dee157a12f
armin <armin10dybala@gmail.com>
2026-08-27 21:13:54 +0330

    config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.systema.music',
   appName: 'SYSTEMA',
   webDir: '.output/public',
-  server: {
-    url: 'http://localhost:3000',
-    cleartext: true
-  }
+  // server: {
+  //   url: 'http://localhost:3000',
+  //   cleartext: true
+  // }
 }
 
 export default config
