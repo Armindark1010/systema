412fcdd668f23da91e1caae3138b1870911225e0
Armindark1010 <112603771+Armindark1010@users.noreply.github.com>
2026-08-28 00:12:33 +0330

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli'
 const config: CapacitorConfig = {
   appId: 'com.systema.music',
   appName: 'SYSTEMA',
-  webDir: '.output/public',
-  server: {
-    url: 'http://localhost:3000',
-    cleartext: true
-  }
+  webDir: '.output/public'
+  // server: {
+  //   url: 'http://localhost:3000',
+  //   cleartext: true
+  // }
 }
 
 export default config
